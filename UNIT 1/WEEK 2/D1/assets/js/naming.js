9b2be24c8ffd19b5ffab43749654892dccb43847
VinceDV <122369775+VinceDV@users.noreply.github.com>
2023-02-06 16:41:03 +0100

    COM

--- UNIT 1/WEEK 2/D1/assets/js/naming.js
+++ UNIT 1/WEEK 2/D1/assets/js/naming.js
@@ -1,9 +1,3 @@
-//var 1 pet_preferito = 'gatto';
-//console.log(1 pet_preferito);
-
-/*var pet = 'gatto';
-var Pet = 'cane';*/
-var PetPreferito = 'coniglio';
 
 var petPreferito = 'criceto';
 console.log(petPreferito);
@@ -14,13 +8,7 @@ console.log($pet);
 var _pet = 'leone';
 console.log(_pet);
 
-var auto = 'scritto da Javascript';
-var output = 'output';
-var _pet2 = 'pantera';
-
 
 document.write('<h1>Iniziamo la nostra pratica</h1>');
 document.write('<p>output scritto da javascript</p>');
 document.write('<p>pantera scritto da javascript</p>');
-
-//var 12 = 'numero';
