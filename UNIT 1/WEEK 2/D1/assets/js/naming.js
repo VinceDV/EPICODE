0aa943c00af33160eee8627dfa70a91d83389b3a
VinceDV <122369775+VinceDV@users.noreply.github.com>
2023-02-06 16:01:04 +0100

    com

--- UNIT 1/WEEK 2/D1/assets/js/naming.js
+++ UNIT 1/WEEK 2/D1/assets/js/naming.js
@@ -3,12 +3,14 @@
 
 /*var pet = 'gatto';
 var Pet = 'cane';*/
-var petPreferito = 'criceto';
 var PetPreferito = 'coniglio';
 
+var petPreferito = 'criceto';
 console.log(petPreferito);
+
 var $pet = 'giraffa';
 console.log($pet);
+
 var _pet = 'leone';
 console.log(_pet);
 
